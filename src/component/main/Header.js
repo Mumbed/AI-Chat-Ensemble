d83f3d36ca738154f802033c78a1704066f2792f
환류상 (가명) <84229112+hynrusang@users.noreply.github.com>
2024-03-05 17:53:54 +0900

    Update Header.js

--- src/component/main/Header.js
+++ src/component/main/Header.js
@@ -1,6 +1,11 @@
-// 메인 페이지 상단 바 컴포넌트.
+/**
+ * 메인 페이지 상단 바 컴포넌트.
+ */
 
+// location 훅을 사용하기 위한 의존성.
 import { useLocation } from "react-router-dom"
+
+// 각 Header의 구성 컴포넌트.
 import Logo from "../sub/header/Logo"
 import PageRouter from "../sub/header/PageRouter"
 
