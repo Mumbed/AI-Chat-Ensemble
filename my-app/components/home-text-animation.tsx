de4d3c300d18445446369b75d31405cde939933d
jihun <quni123@naver.com>
2024-05-15 12:10:41 +0900

    메인제작중 (4.1)

--- my-app/components/home-text-animation.tsx
+++ my-app/components/home-text-animation.tsx
@@ -2,16 +2,17 @@ import React from 'react';
 import styles from './AnimatedText.module.css';
 
 const AnimatedText = () => {
+  
   return (
     <div className = "">
         <div className={styles.content}>
         <div className={styles.content__container}>
-            <p className={styles.content__container__text}>Hello</p>
+            <p className={styles.content__container__text}>내가 질문할 내용은</p>
             <ul className={styles.content__container__list}>
-            <li className={styles.content__container__list__item}>world !</li>
-            <li className={styles.content__container__list__item}>bob !</li>
-            <li className={styles.content__container__list__item}>users !</li>
-            <li className={styles.content__container__list__item}>everybody !</li>
+            <li className={styles.content__container__list__item}>영어</li>
+            <li className={styles.content__container__list__item}>코딩</li>
+            <li className={styles.content__container__list__item}>물리</li>
+            <li className={styles.content__container__list__item}>게임</li>
             </ul>
         </div>
         </div>
