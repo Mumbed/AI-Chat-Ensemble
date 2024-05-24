9499995b62a2a3983e4f9718ad1ac562b59ce745
환류상 (가명) <84229112+hynrusang@users.noreply.github.com>
2024-05-24 23:40:07 +0900

    Update question-sidebar.tsx

--- FrontLine/my-app/components/question-sidebar.tsx
+++ FrontLine/my-app/components/question-sidebar.tsx
@@ -21,6 +21,9 @@ export default function QuestionSidebar({ questions, roomid }: SidebarProps) {
     <section className="h-screen fixed left-0 top-0 bg-gray-100 overflow-y-auto mt-16 dark:bg-zinc-800">
       <nav className="mt-6">
         <div className="px-6 py-3 flex flex-col gap-4">
+          <div className="flex flex-row gap-2">
+            <Button className="w-64" onClick={() => router.push("/ask")}>채팅방 리스트로 이동</Button>
+          </div>
           {questions.map((question, index) => (
             <div key={index} className="flex flex-row gap-2">
               <Button className="w-64" onClick={() => handleQuestionClick(index)}>{question}</Button>
