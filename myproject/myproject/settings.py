bfa4c7fe35906bcf985bad682a52c2b8bb554b37
myeongjincho <chomj12233@gmail.com>
2024-05-24 16:13:42 +0900

    settings.py 주소 추가

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -54,7 +54,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 CORS_ORIGIN_WHITELIST = (
-    'http://127.0.0.1:8000', 'http://localhost:3000')
+    'http://127.0.0.1:8000', 'http://localhost:3000', 'http://43.201.76.43:8000')
 CORS_ALLOW_CREDENTIALS = True
 
 ROOT_URLCONF = 'myproject.urls'
