46c0ee4c59e2014cce72210e3bf263e4c2f4abca
hs-1971423-kimjihun <115778833+hs-1971423-kimjihun@users.noreply.github.com>
2024-05-30 16:36:12 +0900

    Update page.tsx

--- FrontLine/my-app/app/ask/page.tsx
+++ FrontLine/my-app/app/ask/page.tsx
@@ -4,11 +4,6 @@ export default function CounterPage() {
         <div className="flex flex-col min-h-screen">
             <div className="flex flex-row gap-8">
                 {/* 페이지 내용 */}
-<<<<<<< HEAD
-=======
-                <AnswerBox aiSource="gpt" aiResponse="안녕하세요. 무엇을 도와드릴까요?" />
-                <AnswerBox aiSource="gemini" aiResponse="안녕하세요."/>
->>>>>>> 01e694e9705866a1b7fac4f54c850ca560a41991
             </div>
         </div>
 	);
